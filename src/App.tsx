c01877633c445b454f719e1826b25446286cab12
Mostafa-Khalifaa <mostafa.khalifa.2000.mk@gmail.com>
2026-04-12 21:06:06 +0200

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center text-primary-500'>Fundly</h1>
+
+    </>
+  )
+}
+
+export default App
